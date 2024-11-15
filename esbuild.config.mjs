d8dda66c633e772da2e37c0c16830424789d7848
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2024-11-15 14:58:43 -0300

    fix: fixed the build

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
-import esbuild from "esbuild";
-import process from "process";
-import builtins from 'builtin-modules'
+import esbuild from 'esbuild';
+import process from 'process';
+import builtins from 'builtin-modules';
 
 const banner =
 `/*
@@ -12,41 +12,42 @@ if you want to view the source, please visit the github repository of this plugi
 const prod = (process.argv[2] === 'production');
 
 esbuild.build({
-	banner: {
-		js: banner,
-	},
-	entryPoints: ['main.ts'],
-	bundle: true,
-	external: [
-		'obsidian',
-		'electron',
-		'@codemirror/autocomplete',
-		'@codemirror/closebrackets',
-		'@codemirror/collab',
-		'@codemirror/commands',
-		'@codemirror/comment',
-		'@codemirror/fold',
-		'@codemirror/gutter',
-		'@codemirror/highlight',
-		'@codemirror/history',
-		'@codemirror/language',
-		'@codemirror/lint',
-		'@codemirror/matchbrackets',
-		'@codemirror/panel',
-		'@codemirror/rangeset',
-		'@codemirror/rectangular-selection',
-		'@codemirror/search',
-		'@codemirror/state',
-		'@codemirror/stream-parser',
-		'@codemirror/text',
-		'@codemirror/tooltip',
-		'@codemirror/view',
-		...builtins],
-	format: 'cjs',
-	watch: !prod,
-	target: 'es2016',
-	logLevel: "info",
-	sourcemap: prod ? false : 'inline',
-	treeShaking: true,
-	outfile: 'main.js',
+  banner: {
+    js: banner,
+  },
+  entryPoints: ['main.ts'],
+  bundle: true,
+  external: [
+    'obsidian',
+    'electron',
+    '@codemirror/autocomplete',
+    '@codemirror/closebrackets',
+    '@codemirror/collab',
+    '@codemirror/commands',
+    '@codemirror/comment',
+    '@codemirror/fold',
+    '@codemirror/gutter',
+    '@codemirror/highlight',
+    '@codemirror/history',
+    '@codemirror/language',
+    '@codemirror/lint',
+    '@codemirror/matchbrackets',
+    '@codemirror/panel',
+    '@codemirror/rangeset',
+    '@codemirror/rectangular-selection',
+    '@codemirror/search',
+    '@codemirror/state',
+    '@codemirror/stream-parser',
+    '@codemirror/text',
+    '@codemirror/tooltip',
+    '@codemirror/view',
+    ...builtins
+  ],
+  format: 'cjs',
+  watch: !prod,
+  target: 'es2016', // Adjust target based on your plugin needs
+  logLevel: "info",
+  sourcemap: prod ? false : 'inline',
+  treeShaking: true,
+  outfile: 'main.js',
 }).catch(() => process.exit(1));
